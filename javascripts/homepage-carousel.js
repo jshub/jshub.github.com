bbccff033eed35689e056f56bd14c985f42a97f1
fiann <fiann@ohagan.me>
2009-09-09 11:54:28 +0100

    Add functionality for back button

--- javascripts/homepage-carousel.js
+++ javascripts/homepage-carousel.js
@@ -1,15 +1,34 @@
+"use strict";
+/*jslint white: true, browser: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, strict: true, newcap: true, immed: true, indent: 2 */
+
 (function () {
-  var initButtons = function(carousel) {
-    $.each(['jshub', 'owners', 'implementors', 'developers', 'vendors'], function(idx, name) {
-      $('#scroll-'+name).click(function () {
-        $('.carousel-links li').removeClass('active');
-        $(this).addClass('active');
-        carousel.scroll(idx+1);
-      })
+  var panel_numbers = {};
+  var carousel;
+  
+  var scrollToCurrentPanel = function () {
+    var hash = document.location.hash, panel_number = panel_numbers[hash];
+    if (panel_number === undefined) {
+      return;
+    }
+    $('.carousel-links li').removeClass('active');
+    $(hash).addClass('active');
+    carousel.scroll(panel_number);
+  };
+  
+  var initButtons = function (new_carousel) {
+    carousel = new_carousel;
+    $('.carousel-links li').each(function (index, element) {
+      var node = $(this), id = '#' + node.attr('id');
+      panel_numbers[id] = index + 1;
+      
+      node.click(function () {
+        document.location.hash = id;
+        scrollToCurrentPanel();
+      });
     });
   };
   
-  $(document).ready(function() {
+  $(document).ready(function () {
     $('#carousel').jcarousel({
       visible : 1,
       scroll : 1,
@@ -18,6 +37,17 @@
       easing : 'easeOutExpo',
       initCallback : initButtons
     });
+    
+    // poll for back button
+    var prev_label;
+    setInterval(function () {
+      if (document.location.hash === prev_label) {
+        return;
+      } else {
+        prev_label = document.location.hash;
+        scrollToCurrentPanel();
+      }
+    }, 100);
   });
   
-})();
+}());
